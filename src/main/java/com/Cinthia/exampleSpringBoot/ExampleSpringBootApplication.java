1f1b60d8cbca93e2c0cc256ed39108f48e015924
Cinthia Nuñez Salazar <cinthia_sandy@hotmail.com>
2021-12-02 14:26:43 -0400

    Initial commit

---
+++ src/main/java/com/Cinthia/exampleSpringBoot/ExampleSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.Cinthia.exampleSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootApplication.class, args);
+		System.out.println("hola a todos....");
+	}
+
+}
